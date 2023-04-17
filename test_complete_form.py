3c4c71db6438b177154d64a49c920ff9fd309360
Arkadiy Sychkov <Z4mmer@gmail.com>
2023-04-18 00:48:23 +0500

    Добавил гендер, номер телефона и дату рождения

--- test_complete_form.py
+++ test_complete_form.py
@@ -12,3 +12,12 @@ def     test_complete_form():
         browser.element('#firstName').type('Papa').click()
         browser.element('#lastName').type('Carlo').click()
         browser.element('#userEmail').type('PapaCarlo@example.com')
+
+        browser.element('#gender').element('#male')
+
+        browser.element('#userNumber').type('9035645454').click()
+
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__year-select').element('#1997')
+        browser.element('.react-datepicker__month-select').element('#7')
+        browser.element('.react-datepicker__day--009').click()
