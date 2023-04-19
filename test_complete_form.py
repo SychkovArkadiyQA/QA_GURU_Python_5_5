1dd21bbdb60bbfcded92d121521cf4595fbc7bec
Arkadiy Sychkov <Z4mmer@gmail.com>
2023-04-19 14:43:31 +0500

    Add .gitignore

--- test_complete_form.py
+++ test_complete_form.py
@@ -7,10 +7,10 @@ from selene import browser, have, be
 def     test_complete_form():
         browser.config.window_width = 1400
         browser.config.window_height = 1200
-        browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+        browser.config.base_url = 'https://demoqa.com'
 
 
-        browser.open('/')
+        browser.open('/automation-practice-form')
 
         browser.element('#firstName').type('Papa').click()
         browser.element('#lastName').type('Carlo').click()
